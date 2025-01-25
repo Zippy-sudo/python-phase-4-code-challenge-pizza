541bfbc4a81c060010f67f5d3cad4122fe709855
Nehemiah Madahana <nehemiahmadahana230@gmail.com>
2025-01-25 18:58:53 +0300

    Second Trial

--- server/app.py
+++ server/app.py
@@ -24,6 +24,63 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+
+    def get(self):
+        restaurants_dict = [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in Restaurant.query.all()]
+        response = make_response(restaurants_dict, 200)
+        return response
+    
+api.add_resource(Restaurants, '/restaurants')
+
+class RestaurantsByID(Resource):
+
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id = id).first()
+        if restaurant:
+            response = make_response(restaurant.to_dict(), 200)
+            return response
+        else:
+            return make_response({"error": "Restaurant not found"}, 404)
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id = id).first()
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return make_response({},204)
+        else:
+            return make_response({"error": "Restaurant not found"}, 404)
+    
+api.add_resource(RestaurantsByID, '/restaurants/<int:id>')
+
+class Pizzas(Resource):
+
+    def get(self):
+        pizzas_list = [pizza.to_dict() for pizza in Pizza.query.all()]
+        response = make_response(pizzas_list, 200)
+        return response
+    
+api.add_resource(Pizzas, '/pizzas')
+
+class RestaurantPizzas(Resource):
+
+    def post(self):
+                if 1 <= int(request.get_json().get('price')) <= 30 and int(request.get_json().get('restaurant_id')) and int(request.get_json().get('pizza_id')):
+                    to_post = RestaurantPizza(
+                        price=request.get_json().get('price'),
+                        restaurant_id = request.get_json().get('restaurant_id'),
+                        pizza_id = request.get_json().get('pizza_id')
+                        )
+                    db.session.add(to_post)
+                    db.session.commit()
+                    response_dict = to_post.to_dict()
+                    response = make_response(response_dict, 201)
+                else:
+                    response = make_response({"errors": ["validation errors"]},400)
+                return response
+
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -21,8 +21,11 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates="restaurant", cascade='all, delete-orphan')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza', creator=lambda pizza_obj : Pizza(pizza = pizza_obj))
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +39,11 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates="pizza", cascade='all, delete-orphan')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurants', creator=lambda restaurant_obj:Restaurant(restaurant = restaurant_obj))
 
     # add serialization rules
+    serialize_only = ('id', 'name', 'ingredients',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,10 +56,21 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+
+    restaurant = db.relationship('Restaurant', back_populates="restaurant_pizzas")
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_only = ('id', 'pizza.id', 'pizza.ingredients', 'pizza.name', 'pizza_id', 'price', 'restaurant.address', 'restaurant.id', 'restaurant.name', 'restaurant_id', )
 
     # add validation
-
+    @validates('price')
+    def validate_price(self, key, address):
+        if not 0 < int(address) < 31:
+            raise ValueError("Price must be greater than 0 but less than 30")
+        return address
+ 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
